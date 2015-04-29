705bed05656ca100d82d2d42b9c8931f4ee155c5
BujakiAttila <Bujaki.Attila@gmail.com>
2015-04-29 23:58:22 +0200

    hello package - GreetingController - Added simple form example (+Formatted code)   - Added a simple example form with a single field.   - Formatted the code

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,30 +8,46 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class GreetingController
-{
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model)
-    {
-        model.addAttribute("name", name);
-        return "greeting";
-    }
-    
-    // Example for text file download (http://localhost:8080/file)
+public class GreetingController {
+	// Example for simple Hello Word site
+	@RequestMapping("/greeting")
+	public String greeting(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+
+	// Example for text file download (http://localhost:8080/file)
 	@RequestMapping("/file")
 	public void export(HttpServletResponse response) throws IOException {
 
 		String fileContent = "Hello";
 
 		response.setContentType("text/plain");
-		response.setHeader("Content-Disposition", "attachment;filename=myFile.txt");
+		response.setHeader("Content-Disposition",
+				"attachment;filename=myFile.txt");
 		ServletOutputStream out = response.getOutputStream();
 		out.println(fileContent);
 		out.flush();
 		out.close();
+	}
+
+	// Example for simple form (http://localhost:8080/form)
+	@RequestMapping(value = "/form", method = RequestMethod.GET)
+	public String displayName(Model model) {
+		model.addAttribute("Name", "Anonymus");
+		return "form";
+	}
+
+	@RequestMapping(value = "/form", method = RequestMethod.POST)
+	public String submitForm(@RequestParam String Name, Model model) {
+		model.addAttribute("Name", Name);
+		return "form";
 	}
 
 }
