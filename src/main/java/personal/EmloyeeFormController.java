c06ee5db00d5f4906a5df8bb6e1bc7251784695e
BujakiAttila <Bujaki.Attila@gmail.com>
2015-04-30 11:07:54 +0200

    personale package - Implemented Review and Submit function  - Added new templates and beta implementation of controllers (Some things are still missing.)

--- src/main/java/personal/EmloyeeFormController.java
+++ src/main/java/personal/EmloyeeFormController.java
@@ -1,7 +1,9 @@
 package personal;
 
 import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -12,6 +14,26 @@ public class EmloyeeFormController {
 	@RequestMapping("/EmployeeForm")
 	public String EmloyeeForm(){
 		return "EmployeeForm";
+	}
+	
+	// Employee data review - Review Employee data
+	@RequestMapping("/EmployeeReview")
+	public String EmloyeeReview(Model model, HttpServletRequest request){
+		java.util.Enumeration<String> reqEnum = request.getParameterNames();		
+		while (reqEnum.hasMoreElements()) {
+			String paramName = reqEnum.nextElement();
+			String paramValue = request.getParameter(paramName).toString();
+			System.out.println(paramName + "==" + paramValue); // TO DO: Add debug logging instead of println
+			model.addAttribute(paramName, paramValue);
+		}
+ 
+		return "EmployeeReview";
+	}
+	
+	// Employee data submit - Submit Employee data
+	@RequestMapping("/EmployeeSubmit")
+	public String EmloyeeSubmit(Model model){ 
+		return "EmployeeSubmit";
 	}
 	
 	// Exception handling - Display exception information
