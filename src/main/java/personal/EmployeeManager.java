38dc7964cf11275caffd5064f1fb21eb7cc28367
BujakiAttila <Bujaki.Attila@gmail.com>
2015-05-01 12:56:44 +0200

    EmployeeManager - Removed unused imports + Added some comments

--- src/main/java/personal/EmployeeManager.java
+++ src/main/java/personal/EmployeeManager.java
@@ -1,36 +1,31 @@
 package personal;
 
-import hello.Person;
 import hibernate.HibernateUtil;
-
-import java.util.HashMap;
-import java.util.Map;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
-public class EmployeeManager {
-	
-	private Map<Integer, Employee> storage = new HashMap<Integer, Employee>();
-	
+public class EmployeeManager {	
 	private static EmployeeManager instance = new EmployeeManager();  
 	public static EmployeeManager getInstance() {
 		return instance;
 	}
 	
-	int PersistEmployee(Employee employee)
+	// Save employee
+	public int PersistEmployee(Employee employee)
 	{	
-		// persist employee
+		// Persist employee
 		Session session = HibernateUtil.getSession();
 		Transaction transaction = session.beginTransaction();
 		session.save(employee);
 		transaction.commit();
 				
-		// return id (primary key)
+		// Return id (primary key)
 		return employee.getId();
 	}
 	
-	Employee getEmployee(int id)
+	// Load employee with given id.
+	public Employee getEmployee(int id)
 	{
 		Session session = HibernateUtil.getSession();
 		Employee loadedEmployee = (Employee) session.get(Employee.class, id);
