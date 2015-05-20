f3a806979049d660aeef76b062d0a2dd57ce9588
Jonas Gröger <jonas.groeger@gmail.com>
2015-05-20 13:20:37 +0200

    Added EmployeeRepository

---
+++ src/main/java/de/fau/amos4/domain/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package de.fau.amos4.domain;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends CrudRepository<Employee, Long>
+{
+
+}
