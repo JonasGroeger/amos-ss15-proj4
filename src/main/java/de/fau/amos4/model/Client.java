aaa3f4fe3f7d7b388b142111a214afdfff3d8c61
Jonas Gröger <jonas.groeger@gmail.com>
2015-05-25 12:51:49 +0200

    Add toString, hashCode and equals to model classes.

--- src/main/java/de/fau/amos4/model/Client.java
+++ src/main/java/de/fau/amos4/model/Client.java
@@ -1,5 +1,9 @@
 package de.fau.amos4.model;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
 import javax.persistence.*;
 import java.util.List;
 
@@ -46,5 +50,41 @@ public class Client
     public void setCompanyName(String companyName)
     {
         this.companyName = companyName;
+    }
+
+    @Override
+    public String toString()
+    {
+        return new ToStringBuilder(this)
+                .append("id", id)
+                .append("employees", employees)
+                .append("companyName", companyName)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o)
+    {
+        if (this == o) return true;
+
+        if (!(o instanceof Client)) return false;
+
+        Client client = (Client) o;
+
+        return new EqualsBuilder()
+                .append(id, client.id)
+                .append(employees, client.employees)
+                .append(companyName, client.companyName)
+                .isEquals();
+    }
+
+    @Override
+    public int hashCode()
+    {
+        return new HashCodeBuilder(17, 37)
+                .append(id)
+                .append(employees)
+                .append(companyName)
+                .toHashCode();
     }
 }
