d3fc8bd3009a2e49784132cc01d6d386b1e3e35c
Jonas Gröger <jonas.groeger@gmail.com>
2015-05-27 11:41:40 +0200

    Since we import data.sql every time now: Adapt test to that.

--- src/test/java/de/fau/amos4/test/EmployeeTest.java
+++ src/test/java/de/fau/amos4/test/EmployeeTest.java
@@ -2,9 +2,6 @@ package de.fau.amos4.test;
 
 import de.fau.amos4.model.Client;
 import de.fau.amos4.model.Employee;
-import de.fau.amos4.model.fields.Disabled;
-import de.fau.amos4.model.fields.MaritalStatus;
-import de.fau.amos4.model.fields.Sex;
 import de.fau.amos4.service.ClientRepository;
 import de.fau.amos4.service.EmployeeRepository;
 import de.fau.amos4.test.configuration.TestConfiguration;
@@ -17,7 +14,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -70,49 +70,9 @@ public class EmployeeTest
     @Test
     public void testFindEmployeeByClient()
     {
-        // Add client to db
-        Client newClient = new Client();
-        newClient.setId(1l);
-        newClient.setCompanyName("Test Company");
-        newClient = clientRepository.save(newClient);
-
-        // Add employee to db (with client reference)
-        Employee newEmployee = new Employee();
-        newEmployee.setFirstName("firstname");
-        newEmployee.setMaidenName("maidenname");
-        newEmployee.setFamilyName("familyname");
-        newEmployee.setBirthDate(Calendar.getInstance().getTime());
-        newEmployee.setPlaceOfBirth("placeofbirth");
-        newEmployee.setCountryOfBirth("countryofbirth");
-        newEmployee.setStreet("street");
-        newEmployee.setZipCode("1234");
-        newEmployee.setHouseNumber("1234");
-        newEmployee.setCity("city");
-        newEmployee.setSocialInsuranceNumber("socialinsurance");
-        newEmployee.setSex(Sex.UNKNOWN);
-        newEmployee.setMaritalStatus(MaritalStatus.MARRIED);
-        newEmployee.setDisabled(Disabled.NO);
-        newEmployee.setCitizenship("citizenship");
-        newEmployee.setEmployerSocialSavingsNumber("socialsavingsnumber");
-        newEmployee.setIban("iban");
-        newEmployee.setBic("bic");
-        newEmployee.setAdditionToAddress("addition");
-        newEmployee.setToken("token");
-        newEmployee.setClient(newClient);
-        newEmployee = employeeRepository.save(newEmployee);
-
-        // Insert backreference
-        List<Employee> employeeList = new ArrayList<>(1);
-        employeeList.add(newEmployee);
-        newClient.setEmployees(employeeList);
-        clientRepository.save(newClient);
-
         Client c = clientRepository.findOne(1l);
         List<Employee> foundEmployees = employeeRepository.findByClient(c);
 
         Assert.assertEquals("Inserting a employee inserted more than one.", foundEmployees.size(), 1);
-        Assert.assertEquals("Inserted employee is not same as one retrieved from client.",
-                foundEmployees.get(0).getFirstName(), newEmployee.getFirstName());
-        System.out.println(foundEmployees.size());
     }
 }
