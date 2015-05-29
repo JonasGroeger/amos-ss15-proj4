2d6d9eaebfee07de68614a0f4387e3ba6cff0558
BujakiAttila <Bujaki.Attila@gmail.com>
2015-05-29 15:05:27 +0200

    Activate Account(40,41) - Implemented email activation  - Client class: Added activation and email confirmation related fields and methods  - LoginFormController: - Added RegisterClient controller (saving Client and sending confirmation email)                         - Added ClientConfirmation controller (for handling email confirmation requests)

--- src/main/java/de/fau/amos4/model/Client.java
+++ src/main/java/de/fau/amos4/model/Client.java
@@ -2,6 +2,8 @@ package de.fau.amos4.model;
 
 import javax.persistence.*;
 
+import org.apache.commons.lang3.RandomStringUtils;
+
 @Entity
 @Table
 public class Client
@@ -14,7 +16,55 @@ public class Client
     @Column
     String companyName;
 
-    public long getId()
+    @Column
+    Boolean activated;
+
+    @Column
+    String confirmationString;
+    
+    @Column
+    String email;
+
+    public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getActivated() {
+		return activated;
+	}
+
+	public void setActivated(Boolean activated) {
+		this.activated = activated;
+	}
+
+	public String getConfirmationString() {
+		return confirmationString;
+	}
+
+	public void setConfirmationString(String confirmationString) {
+		this.confirmationString = confirmationString;
+	}
+	
+	public void generateConfirmationString() {
+		String RandomString = RandomStringUtils.random(24, "ABCDEFGHIJKLMNOPQRST123456789");
+		this.setConfirmationString(RandomString);
+	}
+	
+	public Boolean tryToActivate(String enteredConfirmationCode)
+	{
+		if(enteredConfirmationCode.equals(this.getConfirmationString()))
+		{
+			this.activated = true;
+		}
+		
+		return this.activated;
+	}
+
+	public long getId()
     {
         return id;
     }
--- src/main/java/de/fau/amos4/web/LoginFormController.java
+++ src/main/java/de/fau/amos4/web/LoginFormController.java
@@ -2,11 +2,24 @@ package de.fau.amos4.web;
 
 import java.util.regex.*;
 
+import javax.annotation.Resource;
+import javax.mail.MessagingException;
+import javax.mail.internet.AddressException;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import de.fau.amos4.model.Client;
+import de.fau.amos4.service.ClientRepository;
+import de.fau.amos4.service.EmployeeRepository;
+import de.fau.amos4.util.EmailSender;
+
 public class LoginFormController {
 
+    @Resource
+    ClientRepository clientRepository;
+    
     @RequestMapping("/Login")
     public String Login(@RequestParam(value = "username", required = true) String userName, @RequestParam(value = "password", required = true) String password)
     {
@@ -45,5 +58,40 @@ public class LoginFormController {
         	// Valid Login -> Redirect to Login Page
     	    return "redirect:/";
         }
+    }
+    
+
+    @RequestMapping("/RegisterClient")
+    public String RegisterClient(HttpServletRequest request, @RequestParam(value = "client", required = true) Client client) throws AddressException, MessagingException
+    {
+    	// Generate new confirmation string for the client
+        client.generateConfirmationString();
+        // Set client to inactive
+        client.setActivated(false);
+    	// Save new, unactivated client
+        clientRepository.save(client);
+        
+        // Prepare and send email
+        String ServerName = request.getServerName();
+        String ConfirmationCode = client.getConfirmationString();
+        String Content = "<a href=" + ServerName + "/ClientConfirmation?id=" + client.getId() + "&confirmation=" + ConfirmationCode + ">";        
+        EmailSender sender = new EmailSender();        
+        sender.SendEmail(client.getEmail(), "PersonalFragebogen 2.0 - Confirmation", Content);
+        
+        // Display login screen after
+        return "RegistrationAlmostReady";
+    }
+
+    @RequestMapping("/ClientConfirmation")
+    public String RegisterClient(@RequestParam(value = "id", required = true) long clientId, @RequestParam(value = "confirmation", required = true) String enteredConfirmationCode) throws AddressException, MessagingException
+    {
+    	 Client client = this.clientRepository.findOne(clientId);
+    	 if(client.tryToActivate(enteredConfirmationCode))
+    	 {
+    		 // Save client after successful activation
+    		 this.clientRepository.save(client);
+    	 }
+    
+         return "redirect:/Login";
     }
 }
