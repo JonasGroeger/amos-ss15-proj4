d8483a8d59ca7d92506acc1fe011e33986977bae
Jonas Gröger <jonas.groeger@gmail.com>
2015-05-31 00:41:16 +0200

    Remove /Login and rename /RegisterClient + /ClientConfirmation

--- src/main/java/de/fau/amos4/web/LoginFormController.java
+++ src/main/java/de/fau/amos4/web/LoginFormController.java
@@ -10,13 +10,16 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
 @Controller
 public class LoginFormController
 {
     private final ClientRepository clientRepository;
+
+    @Autowired
+    private ServletContext servletContext;
 
     @Autowired
     public LoginFormController(ClientRepository clientRepository)
@@ -24,86 +27,36 @@ public class LoginFormController
         this.clientRepository = clientRepository;
     }
 
-    @RequestMapping("/Login")
-    public String Login(@RequestParam(value = "username", required = true) String userName, @RequestParam(value = "password", required = true) String password)
-    {
-    	boolean LoginValid = false;
-    	
-    	// Check username & password: "ClientID", "PassID" format should be accepted. Eg.: "Client32" and "Pass32" are OK. 
-        String patternString = "Client([1-9]*)";
-        Pattern pattern = Pattern.compile(patternString);
-        Matcher matcher = pattern.matcher(userName);
-        boolean matches = matcher.matches();
-        
-        String ClientNumber = "0";
-        
-        if(matches)
-        {
-        	// UserName is in correct format -> check password.
-        	ClientNumber = matcher.group(1);
-        	
-        	// TODO: Implement real username and password handling
-        	boolean PasswordIsValid = password.equals("Pass" + ClientNumber);
-        	if(PasswordIsValid)
-        	{
-        		LoginValid = true;
-        	}
-        	else
-        	{
-        		LoginValid = false;
-        	}
-        }
-    	
-        if(LoginValid)
-        {
-        	// Valid Login -> Redirect to EmployeeList
-    	    return "redirect:/EmployeeList?id=" + ClientNumber;
-        }
-        else
-        {
-        	// Valid Login -> Redirect to Login Page
-    	    return "redirect:/WrongPassword";
-        }
-    }
-    
-    @RequestMapping("/WrongPassword")
-    public String EmployeeLogin()
-    {
-        return "WrongPassword";
-    }
-    
-
-    @RequestMapping("/RegisterClient")
+    @RequestMapping("/client/register")
     public String RegisterClient(HttpServletRequest request, @RequestParam(value = "client", required = true) Client client) throws AddressException, MessagingException
     {
-    	// Generate new confirmation string for the client
+        // Generate new confirmation string for the client
         client.generateConfirmationString();
         // Set client to inactive
         client.setActivated(false);
-    	// Save new, unactivated client
+        // Save new, unactivated client
         clientRepository.save(client);
-        
+
         // Prepare and send email
-        String ServerName = request.getServerName();
+        String contextPath = servletContext.getContextPath();
         String ConfirmationCode = client.getConfirmationString();
-        String Content = "<a href=" + ServerName + "/ClientConfirmation?id=" + client.getId() + "&confirmation=" + ConfirmationCode + ">";        
-        EmailSender sender = new EmailSender();        
-        sender.SendEmail(client.getEmail(), "PersonalFragebogen 2.0 - Confirmation", Content);
-        
+        String Content = "<a href=" + contextPath + "/client/confirm?id=" + client.getId() + "&confirmation=" + ConfirmationCode + ">";
+        EmailSender sender = new EmailSender();
+        sender.SendEmail(client.getEmail(), "Personalragebogen 2.0 - Confirmation", Content);
+
         // Display login screen after
         return "RegistrationAlmostReady";
     }
 
-    @RequestMapping("/ClientConfirmation")
+    @RequestMapping("/client/confirm")
     public String RegisterClient(@RequestParam(value = "id", required = true) long clientId, @RequestParam(value = "confirmation", required = true) String enteredConfirmationCode) throws AddressException, MessagingException
     {
-    	 Client client = this.clientRepository.findOne(clientId);
-    	 if(client.tryToActivate(enteredConfirmationCode))
-    	 {
-    		 // Save client after successful activation
-    		 this.clientRepository.save(client);
-    	 }
-    
-         return "redirect:/Login";
+        Client client = this.clientRepository.findOne(clientId);
+        if (client.tryToActivate(enteredConfirmationCode)) {
+            // Save client after successful activation
+            this.clientRepository.save(client);
+        }
+
+        return "redirect:/client/login";
     }
 }
