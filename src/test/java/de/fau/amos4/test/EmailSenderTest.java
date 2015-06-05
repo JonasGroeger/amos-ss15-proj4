f05ab7e5facb19396661ebd4c734783efdb35b4f
BujakiAttila <Bujaki.Attila@gmail.com>
2015-06-05 23:04:53 +0200

    EmailSenderTest - Removes Spring config for test case  - Simple POJO needs no special Spring setup.

--- src/test/java/de/fau/amos4/test/EmailSenderTest.java
+++ src/test/java/de/fau/amos4/test/EmailSenderTest.java
@@ -3,15 +3,8 @@ package de.fau.amos4.test;
 import org.junit.Assert;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import de.fau.amos4.test.configuration.TestConfiguration;
 import de.fau.amos4.util.EmailSender;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = TestConfiguration.class)
 public class EmailSenderTest {
 
     @Test
