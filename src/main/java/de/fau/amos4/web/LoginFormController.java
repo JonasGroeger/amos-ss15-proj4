eac03c2da6b6350b674205505ae2880084bf5f0d
Jonas Gröger <jonas.groeger@gmail.com>
2015-06-09 14:27:09 +0200

    Add the AGPL license to all *.java files

--- src/main/java/de/fau/amos4/web/LoginFormController.java
+++ src/main/java/de/fau/amos4/web/LoginFormController.java
@@ -1,3 +1,22 @@
+/**
+ * Personalfragebogen 2.0. Revolutionize form data entry for taxation and
+ * other purposes.
+ * Copyright (C) 2015 Attila Bujaki, Werner Sembach, Jonas Gröger, Oswaldo
+ *     Bejarano, Ardhi Sutadi, Nikitha Mohan, Benedikt Rauh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package de.fau.amos4.web;
 
 import de.fau.amos4.model.Client;
