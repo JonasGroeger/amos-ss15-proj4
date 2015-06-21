ef36af98d9728ebf170fd68d6fc6c31cc3966850
Jonas Gröger <jonas.groeger@gmail.com>
2015-06-22 00:05:37 +0200

    Implement correct error handling in the reset password page

---
+++ src/main/java/de/fau/amos4/web/form/ResetPasswordForm.java
@@ -0,0 +1,21 @@
+package de.fau.amos4.web.form;
+
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
+
+public class ResetPasswordForm
+{
+    @NotBlank
+    @Email
+    private String email;
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+}
