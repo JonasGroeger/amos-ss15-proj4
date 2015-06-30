25e2bc765c9cf486233ad1fd980ee5a4c7c16f07
Jonas Gröger <jonas.groeger@gmail.com>
2015-07-01 00:10:22 +0200

    Add the Lodas functionality to the txt file download

--- src/main/java/de/fau/amos4/service/EmployeeService.java
+++ src/main/java/de/fau/amos4/service/EmployeeService.java
@@ -21,13 +21,11 @@ package de.fau.amos4.service;
 
 import de.fau.amos4.model.Employee;
 
-/**
- * Created by Yao Bochao on 07/06/2015.
- */
-public interface EmployeeService {
+public interface EmployeeService
+{
     Employee getEmployeeByToken(String token);
 
-    Iterable<Employee> getAllClients();
-
     Employee create(Employee employee);
+
+    String getLodasRepresentation(long employeeId);
 }
