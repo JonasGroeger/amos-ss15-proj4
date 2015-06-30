249e7c9bfdeec4687b3c62958a0a43c3d38bf25b
Werner Sembach <werner@sembach.de>
2015-07-01 00:06:10 +0200

    Fix typo in data.sql again

--- src/main/java/de/fau/amos4/Application.java
+++ src/main/java/de/fau/amos4/Application.java
@@ -43,7 +43,7 @@ public class Application extends SpringBootServletInitializer
      * config classes) because other settings have sensible defaults. You might choose
      * (for instance) to add default command line arguments, or set an active Spring
      * profile.
-     *
+     * 
      * @param builder a builder for the application context
      * @return the application builder
      * @see org.springframework.boot.builder.SpringApplicationBuilder
