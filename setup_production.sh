d182b5ebf52553c6361f6209a0bc8e5056065a4d
Jonas Gröger <jonas.groeger@gmail.com>
2015-07-01 13:42:01 +0200

    Add bash script to streamline project setup
    Usage: ./setup_production.sh DATABASE USERNAME PASSWORD
    
    This copies the sample production configuration to a usable one
    and configures the values for the database, username and password.

---
+++ setup_production.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+usage()
+{
+	echo "Usage: ./setup_production.sh DATABASE USERNAME PASSWORD"
+}
+
+###############################################################################
+
+SAMPLE_FILE="src/main/resources/config/application-production.yml.sample"
+PROD_FILE="src/main/resources/config/application-production.yml"
+
+if [ -f "$PROD_FILE" ]
+then
+	echo "Script has already been executed."
+	exit 1
+fi
+
+if [ $# -le 2 ]
+then
+    usage
+    exit 1
+fi
+
+cp "$SAMPLE_FILE" "$PROD_FILE"
+sed -i -e "s/<PRODUCTION_DATABASE>/$1/g" "$PROD_FILE"
+sed -i -e "s/<PRODUCTION_USERNAME>/$2/g" "$PROD_FILE"
+sed -i -e "s/<PRODUCTION_PASSWORD>/$3/g" "$PROD_FILE"
+sed -i -e "/^#/d" "$PROD_FILE"
+sed -i -e "1 i\# This is the production specific configuration using the 'production' profile." "$PROD_FILE"
